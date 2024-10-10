36290046618115046f4360fc0e249f91eaf4c77a
joetempleman <2367753+joetempleman@users.noreply.github.com>
2024-10-09 21:46:32 -0700

    fix

--- pi_coffee_scale/run.py
+++ pi_coffee_scale/run.py
@@ -1,22 +1,23 @@
-
 import pyacaia
 import logging
 import time
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 if __name__ == '__main__':
     addresses = pyacaia.find_acaia_devices(backend='pygatt')
+
     if addresses:
-        logger.error("Failed to find devices")
+        logger.debug("Mac addr %s", addresses)        
     else:
-        logger.debug("Mac addr %s", addresses[0])
+        logger.error("Failed to find devices")
+        sys.exit(1)
 
     time.sleep(1)
-    scale=pyacaia.AcaiaScale()
+    scale=pyacaia.AcaiaScale(addresses[0], backend='pygatt')
 
-    scale.connect(addresses[0])
+    scale.connect()
 
     for i in range(10):
         print(scale.weight)
