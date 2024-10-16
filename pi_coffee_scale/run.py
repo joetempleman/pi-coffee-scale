97aa7f7022e3c01e4ee77520dc17091a21f4e5b4
Joe Templeman <2367753+joetempleman@users.noreply.github.com>
2024-10-16 13:56:10 -0700

    fixes

--- pi_coffee_scale/run.py
+++ pi_coffee_scale/run.py
@@ -1,7 +1,8 @@
 import pyacaia
 import logging
 import time
-from pygatt import GATTToolBackend 
+from pygatt import GATTToolBackend
+from pygatt.exceptions import NotConnectedError
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -18,7 +19,23 @@ if __name__ == '__main__':
     # addr = addresses[0]
     def handle(handle, value): 
         print(int(''.join(([str(v - 48) for v in value[3:8]]))) / 10)
-
-    from pygatt import GATTToolBackend
-    adapter = GATTToolBackend('hci0'); adapter.reset(); adapter.start(); d = adapter.connect("68:5E:1C:15:BC:F7")
+    addr = "68:5E:1C:15:BC:F7"
+    logger.info("Getting Adapter")
+    adapter = GATTToolBackend('hci0'); 
+    logger.info("Getting Adapter")
+    adapter.reset(); 
+    logger.info("Starting Adapter")
+    adapter.start(); 
+    logger.info("Connecting to %s", addr)
+    tries = 0
+    d = None
+    while d == None:
+        try:
+            d = adapter.connect(addr)
+        except NotConnectedError:
+            if tries < 5:    
+                logger.error("Failed to connect, retrying")
+                tries += 1
+            else:
+                raise
     d.subscribe('0000ffe1-0000-1000-8000-00805f9b34fb', handle)
