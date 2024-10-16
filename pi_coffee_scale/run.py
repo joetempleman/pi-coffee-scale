c33fd226df5fe145da2e3a854b72f1c29af1230e
Joe Templeman <2367753+joetempleman@users.noreply.github.com>
2024-10-16 13:49:25 -0700

    fixes

--- pi_coffee_scale/run.py
+++ pi_coffee_scale/run.py
@@ -1,28 +1,24 @@
 import pyacaia
 import logging
 import time
+from pygatt import GATTToolBackend 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 if __name__ == '__main__':
-    addresses = pyacaia.find_acaia_devices(backend='pygatt')
+    # addresses = pyacaia.find_acaia_devices(backend='pygatt')
 
-    if addresses:
-        logger.debug("Mac addr %s", addresses)        
-    else:
-        logger.error("Failed to find devices")
-        sys.exit(1)
+    # if addresses:
+    #     logger.debug("Mac addr %s", addresses)        
+    # else:
+    #     logger.error("Failed to find devices")
+    #     sys.exit(1)
 
-    time.sleep(1)
-    scale=pyacaia.AcaiaScale(addresses[0], backend='pygatt')
+    # addr = addresses[0]
+    def handle(handle, value): 
+        print(int(''.join(([str(v - 48) for v in value[3:8]]))) / 10)
 
-    scale.connect()
-
-    for i in range(10):
-        print(scale.weight)
-        time.sleep(0.5)
-
-    scale.disconnect()
-
-
+    from pygatt import GATTToolBackend
+    adapter = GATTToolBackend('hci0'); adapter.reset(); adapter.start(); d = adapter.connect("68:5E:1C:15:BC:F7")
+    d.subscribe('0000ffe1-0000-1000-8000-00805f9b34fb', handle)
