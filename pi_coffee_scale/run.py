2471c6af0052bf9087a04d8a8e6327f7f441c31d
Joe Templeman <2367753+joetempleman@users.noreply.github.com>
2024-10-25 07:30:15 -0700

    Fix buffer

--- pi_coffee_scale/run.py
+++ pi_coffee_scale/run.py
@@ -11,7 +11,7 @@ from gpiozero import OutputDevice, Button
 from pygatt.backends.gatttool import device
 from queue import Queue
 
-WEIGHT_BUFFER = 1
+WEIGHT_BUFFER = 0.45
 
 BASE_UUID = "0000%s-0000-1000-8000-00805f9b34fb"
 DATA_SERVICE = BASE_UUID % "ffe0"
